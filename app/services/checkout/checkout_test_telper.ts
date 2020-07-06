c63c77c9ce85f530b974034c26a14d242537f896
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2020-07-06 15:32:34 +0700

    Add checkout test helper

---
+++ app/services/checkout/checkout_test_telper.ts
@@ -0,0 +1,21 @@
+import * as MongoDb from 'mongodb'
+import { CheckoutAbleCart, CheckoutItem } from './checkout_type'
+import * as ProductTestHelper from '../product/product_test_helper'
+
+export function generateCheckoutAbleCart(props: Partial<CheckoutAbleCart>): CheckoutAbleCart {
+  return {
+    id: new MongoDb.ObjectId(),
+    items: [
+      generateCheckoutItem({ quantity: 3, unitPrice: 300 }),
+      generateCheckoutItem({ quantity: 4, unitPrice: 100 })
+    ]
+  }
+}
+
+export function generateCheckoutItem(props: Partial<CheckoutItem>): CheckoutItem {
+  return {
+    ...ProductTestHelper.generateMockProduct({ unitPrice: 300 }),
+    quantity: 3,
+    ...props
+  }
+}
